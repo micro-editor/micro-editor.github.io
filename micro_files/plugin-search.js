615e9c431b6fc2d4a1eaf3479fbeeb184aa47c71
Zachary Yedidia <zyedidia@gmail.com>
2020-02-10 14:26:51 -0500

    Improve plugin page
    The displayed version is always the most recent, and expanded views
    give a link to the plugin website if it exists.

--- micro_files/plugin-search.js
+++ micro_files/plugin-search.js
@@ -3,64 +3,111 @@
 var searchResults = [];
 var pluginData;
 
+function versionCompare(v1, v2, options) {
+	var lexicographical = options && options.lexicographical,
+		zeroExtend = options && options.zeroExtend,
+		v1parts = v1.split('.'),
+		v2parts = v2.split('.');
+
+	function isValidPart(x) {
+		return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
+	}
+
+	if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+		return NaN;
+	}
+
+	if (zeroExtend) {
+		while (v1parts.length < v2parts.length) v1parts.push("0");
+		while (v2parts.length < v1parts.length) v2parts.push("0");
+	}
+
+	if (!lexicographical) {
+		v1parts = v1parts.map(Number);
+		v2parts = v2parts.map(Number);
+	}
+
+	for (var i = 0; i < v1parts.length; ++i) {
+		if (v2parts.length == i) {
+			return 1;
+		}
+
+		if (v1parts[i] == v2parts[i]) {
+			continue;
+		}
+		else if (v1parts[i] > v2parts[i]) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+
+	if (v1parts.length != v2parts.length) {
+		return -1;
+	}
+
+	return 0;
+}
+
 function fetchData() {
-    fetch('https://raw.githubusercontent.com/micro-editor/plugin-channel/master/channel.json').then(function (response) {
-        return response.text().then(function (text) {
-            // Remove Comments
-            var rawdata = text.replace(/[/s][\/]\B.+/g, "");
-            console.log(rawdata);
-            // Parse Data
-            var channel = JSON.parse(rawdata);
-            pluginData = [];
-            channel.forEach(function (item) {
-                fetch(item).then(function (response) {
-                    return response.json().then(function (json) {
-                        pluginData.push(json[0]);
-                        console.log(json[0]);
-                        search(true);
-                    });
-                });
-            });
-        });
+	fetch('https://raw.githubusercontent.com/micro-editor/plugin-channel/master/channel.json').then(function (response) {
+		return response.text().then(function (text) {
+			// Remove Comments
+			var rawdata = text.replace(/[/s][\/]\B.+/g, "");
+			console.log(rawdata);
+			// Parse Data
+			var channel = JSON.parse(rawdata);
+			pluginData = [];
+			channel.forEach(function (item) {
+				fetch(item).then(function (response) {
+					return response.json().then(function (json) {
+						pluginData.push(json[0]);
+						console.log(json[0]);
+						search(true);
+					});
+				});
+			});
+		});
 });
 }
 
 function search(collapse) {
-    switch (document.querySelector('input[name="searchby"]:checked').value) {
-        case 'Name':
-            console.log('Search by Name');
-            searchResults = pluginData.filter(function (item) {
-                return item.Name.includes(document.getElementById('keyword').value);
-            });
-            console.log(searchResults);
-            showResults(searchResults, collapse);
-            break;
-        case 'Description':
-            console.log('Search by Description');
-            searchResults = pluginData.filter(function (item) {
-                return item.Description.includes(document.getElementById('keyword').value);
-            });
-            console.log(searchResults);
-            showResults(searchResults, collapse);
-            break;
-        case 'Tags':
-            console.log('Search by Tags');
-            searchResults = pluginData.filter(function (item) {
-                return item.Tags.includes(document.getElementById('keyword').value);
-            });
-            console.log(searchResults);
-            showResults(searchResults, collapse);
-            break;
-    }
+	switch (document.querySelector('input[name="searchby"]:checked').value) {
+		case 'Name':
+			console.log('Search by Name');
+			searchResults = pluginData.filter(function (item) {
+				return item.Name.includes(document.getElementById('keyword').value);
+			});
+			console.log(searchResults);
+			showResults(searchResults, collapse);
+			break;
+		case 'Description':
+			console.log('Search by Description');
+			searchResults = pluginData.filter(function (item) {
+				return item.Description.includes(document.getElementById('keyword').value);
+			});
+			console.log(searchResults);
+			showResults(searchResults, collapse);
+			break;
+		case 'Tags':
+			console.log('Search by Tags');
+			searchResults = pluginData.filter(function (item) {
+				return item.Tags.includes(document.getElementById('keyword').value);
+			});
+			console.log(searchResults);
+			showResults(searchResults, collapse);
+			break;
+	}
 }
 
 function showResults(results, collapsed) {
-    var table = document.getElementById('results');
-    table.innerHTML = '';
-    var collapseStr = "panel-collapse collapse";
-    if (!collapsed) {
-        collapseStr += " in";
-    }
+	var table = document.getElementById('results');
+	table.innerHTML = '';
+	var collapseStr = "panel-collapse collapse";
+	if (!collapsed) {
+		collapseStr += " in";
+	}
 
  		function separate_with_commas(tar_array) {
  			let results = "";
@@ -74,7 +121,7 @@ function showResults(results, collapsed) {
  			return results;
  		}
 
-    results.forEach(function (item) {
+	results.forEach(function (item) {
 			// The displayed tag(s)
 			let display_item_tag = separate_with_commas(item.Tags);
 			// The displayed version(s)
@@ -83,6 +130,7 @@ function showResults(results, collapsed) {
 			let display_item_require = '';
 			let current_requires = [];
 			let requires_len = 0;
+			let latestVers = "0.0.0"
 
 			// Handle  multiple versions in repo.json
 			for (let i = 0; i < item.Versions.length; i++) {
@@ -114,6 +162,10 @@ function showResults(results, collapsed) {
 					// Linebreak so we display them below eachother
 					display_item_require += '\n';
 				}
+
+				if (versionCompare(latestVers, item.Versions[i].Version) < 0) {
+					latestVers = item.Versions[i].Version;
+				}
 			}
 
 			// If there's only a single version, don't show the "All versions: " thing
@@ -124,6 +176,11 @@ function showResults(results, collapsed) {
 				display_item_allversions += '</p>';
 			}
 
-        table.innerHTML += '<div class="panel panel-default">\n        <div class="panel-heading" role="tab" id="h-' + item.Name + '">\n          <h4 class="panel-title">\n            <a role="button" data-toggle="collapse" data-parent="#results"\n              href="#c-' + item.Name + '" aria-controls="c-' + item.Name + '">\n              ' + item.Name + ' ' + item.Versions[item.Versions.length - 1].Version + '\n            </a>\n          </h4>\n        </div>\n        <div id="c-' + item.Name + '" class="' + collapseStr + '" role="tabpanel" aria-labelledby="h-' + item.Name + '">\n          <div class="panel-body">\n            <p>' + item.Description + '</p>\n            <p><span class="glyphicon glyphicon-tags" aria-hidden="true"></span> ' + display_item_tag + display_item_allversions + display_item_require + '</p>\n            <p>To install this plugin, open micro from your CLI,\n            press [Crtl + E] then run the command line below.\n            Once you are done, restart micro.</p>\n            <div class="well">&gt; plugin install ' + item.Name + '</div>\n          </div>\n        </div>\n      </div>';
-    });
+		var website = "";
+		if (item.Website != undefined) {
+			website = '<a href="' + item.Website + '">' + item.Website + '</a><br>';
+		}
+
+		table.innerHTML += '<div class="panel panel-default">\n        <div class="panel-heading" role="tab" id="h-' + item.Name + '">\n          <h4 class="panel-title">\n            <a role="button" data-toggle="collapse" data-parent="#results"\n              href="#c-' + item.Name + '" aria-controls="c-' + item.Name + '">\n              ' + item.Name + ' ' + latestVers + '\n            </a>\n          </h4>\n        </div>\n        <div id="c-' + item.Name + '" class="' + collapseStr + '" role="tabpanel" aria-labelledby="h-' + item.Name + '">\n          <div class="panel-body">\n            <p>' + item.Description + '</p>\n' + website + '\n            <p><span class="glyphicon glyphicon-tags" aria-hidden="true"></span> ' + display_item_tag + display_item_allversions + display_item_require + '</p>\n            <p>To install this plugin, run the following command from your CLI</p>\n            <div class="well"$ micro -plugin install ' + item.Name + '</div>\n          </div>\n        </div>\n      </div>';
+	});
 }
